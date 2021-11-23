d5c6a6ee1ff01076fb217219d507b6b0e4f286fc
Sebastian Will <swill@csail.mit.edu>
2021-11-23 12:52:37 +0100

    Remove old main page defined in c++ code

--- src/infrared/infrared.hpp
+++ src/infrared/infrared.hpp
@@ -2,19 +2,6 @@
 #define INFRARED_HPP
 
 /**
- * @mainpage
- * InfraRed -- A generic framework for Dynamic Programming, Optimization and Boltzmann 
- * sampling over constraint models
- *
- * (C) Sebastian Will, 2018--2021
- *
- * InfraRed provides a generic framework for tree decomposition-based
- * Dynamic Programming, including optimization and Boltzmann sampling
- * based on constraint models.
- *
- */
-
-/**
  * @file
  * @brief Includes all headers of the (header-only) Infrared core library
  */
