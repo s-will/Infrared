b383d950d1d10923385ac24fc031544088f9f0d3
Sebastian Will <swill@csail.mit.edu>
2023-07-06 22:18:03 +0200

    Fix notebook documentation

--- docIpynbFilter.sh
+++ docIpynbFilter.sh
@@ -8,5 +8,10 @@ jupyter nbconvert --to markdown "$target"
 
 echo [TOC]
 echo
-cat "${target/.ipynb/.md}" | sed 's/```python/```{.py}/g' | sed 's/^##/#/g' | sed 's/\\(\(.*\)\\)/\\f$\1\\f$/g;s/\\\[/\\f[/g;s/\\\]/\\f]/g;s/$$\(.\+\)$\$/\\f[\1\\f]/g;s/$\([^$]\+\)\$/\\f$\1\\f$/g' | tail -n +2
 
+cat "${target/.ipynb/.md}" \
+  | sed 's/```python/```{.py}/g' \
+  | sed 's/@/@ /g' \
+  | sed 's/^##/#/g' \
+  | sed 's/\\(\(.*\)\\)/\\f$\1\\f$/g;s/\\\[/\\f[/g;s/\\\]/\\f]/g;s/$$\(.\+\)$\$/\\f[\1\\f]/g;s/$\([^$]\+\)\$/\\f$\1\\f$/g' \
+  | tail -n +2
